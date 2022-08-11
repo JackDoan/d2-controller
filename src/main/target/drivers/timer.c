021cf63406362a026ff8611fe5dc04e67a77c042
Jack Doan <jackdoan@rivian.com>
2022-08-11 12:07:38 -0500

    cleanup

---
+++ src/main/target/drivers/timer.c
@@ -0,0 +1,5 @@
+//
+// Created by jack on 8/11/22.
+//
+
+#include "timer.h"
---
+++ src/main/target/drivers/timer.h
@@ -0,0 +1,8 @@
+//
+// Created by jack on 8/11/22.
+//
+
+#ifndef INAV_TIMER_H
+#define INAV_TIMER_H
+
+#endif //INAV_TIMER_H
